e46072b0776ae4260c4de7f6e618ea1213188981
Chloe2330 <chloeiscalm@gmail.com>
2023-05-11 00:49:32 -0700

    build API endpoints (url generation, redirection)

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/Chloe2330/go-url-shortener/handler"
+	"github.com/Chloe2330/go-url-shortener/store"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,6 +23,17 @@ func main() {
 		// gin.H{} creates a map with string key ("message") and a corresponding value 
 	})
 
+	router.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	router.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// store initialization
+	store.InitializeStore()
+
 	// starts HTTP server and listens on port 8080
 	err := router.Run(":8080")
 
@@ -29,3 +42,22 @@ func main() {
 		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
 	}
 }
+
+// curl command (WSL Ubuntu)
+/*
+curl -X POST -H "Content-Type: application/json" -d '{
+    "long_url": "https://www.digitalocean.com/community/tutorials/how-to-build-a-ruby-on-rails-application#step-5-adding-validations",
+    "user_id": "e0dba740-fc4b-4977-872c-d360239e6b10"
+}' http://localhost:8080/create-short-url
+*/
+
+// useful Redis commands 
+/* 
+sudo service redis-server stop 
+sudo service redis-server start 
+KEYS *
+GET <key>
+DEL <key>
+TIL <key> 
+EXPIRE <key> seconds
+*/
